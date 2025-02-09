b96c9a89c109e08c1a5fc93bb1b9dc795b69c9d0
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-02-09 20:49:52 +0500

    style: enhance product card layout and image styling

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Product({ product, deleteBtn }) {
   return (
     <li className="productCard">
-      {/* <img src={product.images[0]} alt="" width={200} /> */}
+      <img className="productCard__img" src={product.images[0]} alt="" width={200} />
       <h2 className="productCard__title">{product.title}</h2>
       <p className="productCard__category">Category: {product.category}</p>
       <button onClick={() => deleteBtn(product.id)}>Delete</button>
